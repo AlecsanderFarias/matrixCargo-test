63407bf6740d3aeac72ec8bd9baed5ddd263a714
AlecsanderFarias <alecs@devfarias.com>
2021-03-30 20:12:12 -0300

    remove Job method

--- backend/src/controllers/Job/methods/remove.js
+++ backend/src/controllers/Job/methods/remove.js
@@ -1,8 +1,22 @@
 const exception = require('../../../middlewares/catchException');
 
+import Job from '../../../models/Job';
+
 const remove = async (req, res) => {
   try {
-    return res.status(200).json({});
+    const { id } = req.params;
+
+    const job = await Job.findById(id);
+
+    if (!job) {
+      return res.status(401).json({
+        error: 'Job not found.',
+      });
+    }
+
+    await Job.findByIdAndDelete(id);
+
+    return res.status(200).end();
   } catch (error) {
     exception(error);
     return res.status(500).json(error);
