6e9b272eaa9a7b80e20c9c9829fb4e06e8a900ba
AlecsanderFarias <alecs@devfarias.com>
2021-03-30 20:11:19 -0300

    create Job method

--- backend/src/controllers/Job/methods/create.js
+++ backend/src/controllers/Job/methods/create.js
@@ -1,8 +1,14 @@
 const exception = require('../../../middlewares/catchException');
 
+import Job from '../../../models/Job';
+
 const create = async (req, res) => {
   try {
-    return res.status(200).json({});
+    const { body } = req;
+
+    const job = await Job.create(body);
+
+    return res.status(200).json(job);
   } catch (error) {
     exception(error);
     return res.status(500).json(error);
