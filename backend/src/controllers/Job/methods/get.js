674bf940dbb8dfe5007c2f85408503f577a6d401
AlecsanderFarias <alecs@devfarias.com>
2021-03-30 20:11:31 -0300

    get Job method

--- backend/src/controllers/Job/methods/get.js
+++ backend/src/controllers/Job/methods/get.js
@@ -1,8 +1,20 @@
 const exception = require('../../../middlewares/catchException');
 
+import Job from '../../../models/Job';
+
 const get = async (req, res) => {
   try {
-    return res.status(200).json({});
+    const { id } = req.params;
+
+    const job = await Job.findById(id);
+
+    if (!job) {
+      return res.status(401).json({
+        error: 'Job not found.',
+      });
+    }
+
+    return res.status(200).json(job);
   } catch (error) {
     exception(error);
     return res.status(500).json(error);
